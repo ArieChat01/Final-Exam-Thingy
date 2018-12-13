2affdf5643b537c582aa0180eac2b0ca0ac9c8d0
Unknown <arieauna.chatmon.2019@student.lhschools.org>
2018-12-13 14:20:05 -0500

    oohhh...

--- src/RANDOM.java
+++ src/RANDOM.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class RANDOM {
+    public static void main(String [] ranndom){
+        String ingredients;
+        String cups;
+        String days;
+        boolean program = true;
+        Scanner userResponse;
+        userResponse = new Scanner(System.in);
+        String person;
+        int personIngredients;
+        int unit;
+        int amount;
+        int totalOne;
+
+        System.out.println("WELCOME TO THE PROGRAM!");
+        System.out.println("How many supply objects do you have to input?");
+        personIngredients = userResponse.nextInt();
+        for(int i = 0; i == personIngredients; i++){
+            System.out.println("Name of ingredient: ");
+            person = userResponse.nextLine();
+            System.out.println("Price per unit of ingredient:");
+            unit = userResponse.nextInt();
+            System.out.println("Amount bought of item:");
+            amount = userResponse.nextInt();
+
+
+        }
+    }
 }
