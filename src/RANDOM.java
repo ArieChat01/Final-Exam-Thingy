f60903faa00fda29d4ebbc6363cea3519ecf2180
Unknown <arieauna.chatmon.2019@student.lhschools.org>
2018-12-14 14:28:05 -0500

    help...

--- src/RANDOM.java
+++ src/RANDOM.java
@@ -2,30 +2,49 @@ import java.util.Scanner;
 
 public class RANDOM {
     public static void main(String [] ranndom){
-        String ingredients;
-        String cups;
-        String days;
+
         boolean program = true;
         Scanner userResponse;
         userResponse = new Scanner(System.in);
         String person;
+        String blank;
         int personIngredients;
-        int unit;
-        int amount;
-        int totalOne;
+        int personDays;
+        float cups;
+        float cupPrice;
+        float unit;
+        float amount;
+        float totalOne;
+        float totalTwo;
+        float total;
+        float totalThree;
 
         System.out.println("WELCOME TO THE PROGRAM!");
         System.out.println("How many supply objects do you have to input?");
         personIngredients = userResponse.nextInt();
-        for(int i = 0; i == personIngredients; i++){
-            System.out.println("Name of ingredient: ");
-            person = userResponse.nextLine();
-            System.out.println("Price per unit of ingredient:");
-            unit = userResponse.nextInt();
-            System.out.println("Amount bought of item:");
-            amount = userResponse.nextInt();
-
-
+        while (program) {
+            for (int i = 0; i < personIngredients; i++) {
+                System.out.println("Name of ingredient: ");
+                blank = userResponse.nextLine();
+                person = userResponse.nextLine();
+                System.out.println("Price per unit of ingredient:");
+                unit = userResponse.nextFloat();
+                System.out.println("Amount bought of item:");
+                amount = userResponse.nextFloat();
+                totalOne = unit * amount;
+                System.out.println(totalOne);
+            }
+                System.out.println("How many days did you sell for?");
+                personDays = userResponse.nextInt();
+                for (int q = 0; q < personDays; q++) {
+                    System.out.println("How many cups did you sell on day:" + q);
+                    cups = userResponse.nextFloat();
+                    System.out.println("How much did you sell each cup for?");
+                    cupPrice = userResponse.nextFloat();
+                    totalTwo = cups * cupPrice;
+                    totalTwo = totalTwo + totalTwo;
+                    System.out.println(totalTwo);
+                }
         }
     }
 }
